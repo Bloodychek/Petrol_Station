1701b6fa3623c7a6b925db9c833b0a6a1837c076
Евгений Егоров <andryushenka_egorov_2015@mail.ru>
2020-10-13 15:49:57 +0300

    Добавьте файлы проекта.

---
+++ Petrol_Station/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Petrol_Station.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
